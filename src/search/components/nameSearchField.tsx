7fafb0e4d7c5067b5fbd67801a987c5d948e9805
Dpalme <diegoPalmerin24@gmail.com>
2024-02-28 02:25:51 -0600

    fixed bad route

--- src/search/components/nameSearchField.tsx
+++ src/search/components/nameSearchField.tsx
@@ -15,7 +15,7 @@ export const NameSearchField = () => {
       action="/buscar"
       method="get"
       onSubmit={handleSubmit((values) => {
-        navigate({ to: '/buscar', search: { name: values.search } });
+        navigate({ to: '/', search: { name: values.search } });
       })}
       className="grid grid-cols-[1fr_min-content] lg:grid-cols-[1fr_min-content]
       gap-4 justify-items-stretch flex-grow w-full
