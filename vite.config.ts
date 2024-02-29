75d3e444c2a18f810a89a24874ee1757f9d1f980
Dpalme <diegoPalmerin24@gmail.com>
2024-02-28 22:00:14 -0600

    updated job to use base_url

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-  resolve: {
-    alias: {
-      '@assets': path.resolve(
-        path.dirname(fileURLToPath(import.meta.url)),
-        'src/assets'
-      ),
-      '@modules': path.resolve(
-        path.dirname(fileURLToPath(import.meta.url)),
-        'src/modules'
-      ),
-      '@shared': path.resolve(
-        path.dirname(fileURLToPath(import.meta.url)),
-        'src/shared'
-      ),
-      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src'),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), TanStackRouterVite()],
+    base: env.BASE_URL,
+    resolve: {
+      alias: {
+        '@assets': path.resolve(
+          path.dirname(fileURLToPath(import.meta.url)),
+          'src/assets'
+        ),
+        '@modules': path.resolve(
+          path.dirname(fileURLToPath(import.meta.url)),
+          'src/modules'
+        ),
+        '@shared': path.resolve(
+          path.dirname(fileURLToPath(import.meta.url)),
+          'src/shared'
+        ),
+        '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src'),
+      },
     },
-  },
+  };
 });
